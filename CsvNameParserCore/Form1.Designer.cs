73ea1115fe46621926f6e68462efa00da3754740
Ted Krapf <54634091+tedkrapf@users.noreply.github.com>
2020-11-02 16:07:26 -0600

    initial commit

---
+++ CsvNameParserCore/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace CsvNameParserCore
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.lblSelectedFilename = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ddlColumnToParse = new System.Windows.Forms.ComboBox();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(38, 36);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(161, 23);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Select CSV File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // lblSelectedFilename
+            // 
+            this.lblSelectedFilename.AutoSize = true;
+            this.lblSelectedFilename.Location = new System.Drawing.Point(149, 87);
+            this.lblSelectedFilename.Name = "lblSelectedFilename";
+            this.lblSelectedFilename.Size = new System.Drawing.Size(113, 15);
+            this.lblSelectedFilename.TabIndex = 1;
+            this.lblSelectedFilename.Text = "[no file selected yet]";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selected Filename:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Column to Parse:";
+            // 
+            // ddlColumnToParse
+            // 
+            this.ddlColumnToParse.FormattingEnabled = true;
+            this.ddlColumnToParse.Location = new System.Drawing.Point(149, 108);
+            this.ddlColumnToParse.Name = "ddlColumnToParse";
+            this.ddlColumnToParse.Size = new System.Drawing.Size(209, 23);
+            this.ddlColumnToParse.TabIndex = 4;
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(47, 167);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(152, 23);
+            this.btnParse.TabIndex = 5;
+            this.btnParse.Text = "Parse File\'s Column";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Status Messages:";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(28, 252);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtStatus.Size = new System.Drawing.Size(750, 330);
+            this.txtStatus.TabIndex = 7;
+            // 
+            // progBar
+            // 
+            this.progBar.Location = new System.Drawing.Point(28, 604);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(750, 23);
+            this.progBar.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 641);
+            this.Controls.Add(this.progBar);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.ddlColumnToParse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSelectedFilename);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Label lblSelectedFilename;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ddlColumnToParse;
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progBar;
+    }
+}
+
---
+++ CsvNameParserCore/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Dynamic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BinaryFog.NameParser;
+using CsvHelper;
+
+namespace CsvNameParserCore
+{
+    public partial class Form1 : Form
+    {
+        private int TotalRecordsInCsvFile = 0;
+        private BackgroundWorker bgWorker_BinaryFog;
+
+        private class BackgroundParameters
+        {
+            public string FilePath { get; set; }
+            public string ColumnNameToParse { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+            bgWorker_BinaryFog = new BackgroundWorker();
+            bgWorker_BinaryFog.WorkerReportsProgress = true;
+            bgWorker_BinaryFog.DoWork += new System.ComponentModel.DoWorkEventHandler(bgWorker_BinaryFog_DoWork);
+            bgWorker_BinaryFog.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(bgWorker_BinaryFog_ProgressChanged);
+            bgWorker_BinaryFog.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(bgWorker_BinaryFog_RunWorkerCompleted);
+            
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 1;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    lblSelectedFilename.Text = openFileDialog1.FileName;
+
+                    using (var reader = new StreamReader(openFileDialog1.FileName))
+                    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                    {
+                        var records = csv.GetRecords<dynamic>();
+                        var headers = records.First();
+                        List<string> hs = new List<string>();
+                        foreach (var h in headers)
+                            hs.Add(h.Key.ToString());
+                        ddlColumnToParse.DataSource = hs;
+                        TotalRecordsInCsvFile = records.Count();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                txtStatus.Text = "ERROR: " + ex.Message + Environment.NewLine + ex.ToString()
+                    + Environment.NewLine
+                    + "-----------------------------------"
+                    + Environment.NewLine
+                    + txtStatus.Text;
+            }
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            btnParse.Enabled = false;
+
+            btnParse.Text = "processing";
+            btnParse.Text = "... processing, please wait ...";
+
+            //progBar.Value = 0;
+
+            BackgroundParameters bp = new BackgroundParameters();
+            bp.FilePath = lblSelectedFilename.Text;
+            bp.ColumnNameToParse = ddlColumnToParse.SelectedValue.ToString();
+            txtStatus.Text += Environment.NewLine + "... working on column: " + bp.ColumnNameToParse
+                + Environment.NewLine + "... working on file: " + bp.FilePath
+                + Environment.NewLine + "... number of rows to parse: " + TotalRecordsInCsvFile.ToString();
+
+            bgWorker_BinaryFog.RunWorkerAsync(bp);
+        }
+
+        private void bgWorker_BinaryFog_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var bgParams = (BackgroundParameters)e.Argument;
+
+            var outputRows = new List<dynamic>();
+            int cntRows = 0;
+            int cntParsed = 0;
+            int cntErrored = 0;
+            string errs = Environment.NewLine + "ERRORS: ";
+            string msg = "";
+            string haltMsg = "";
+
+            try
+            {
+                using (var reader = new StreamReader(bgParams.FilePath))
+                {
+                    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                    {
+                        var csvRows = csv.GetRecords<dynamic>();
+                        foreach (var csvRow in csvRows)
+                        {
+                            cntRows++;
+
+                            int percentage = (cntRows) * 100 / (TotalRecordsInCsvFile - 1);
+
+                            bgWorker_BinaryFog.ReportProgress(percentage);
+
+                            dynamic newRow = new ExpandoObject();
+                            newRow = DeepCopy(csvRow);
+
+                            foreach (var column in csvRow)
+                            {
+                                try
+                                {
+                                    if (column.Key.ToString() == bgParams.ColumnNameToParse.ToString())
+                                    {
+
+                                        BinaryFog.NameParser.FullNameParser target = new BinaryFog.NameParser.FullNameParser(column.Value.ToString());
+                                        target.Parse();
+                                        newRow._ParsedNameValueToTheRight = " ~~~> ";
+                                        newRow._CompanyName = "";
+                                        newRow._FirstName = target.FirstName;
+                                        newRow._MiddleName = target.MiddleName;
+                                        newRow._LastName = target.LastName;
+                                        newRow._Title = target.Title;
+                                        newRow._NickName = target.NickName;
+                                        newRow._Suffix = target.Suffix;
+                                        newRow._DisplayName = target.DisplayName;
+                                        newRow._ParseScore = target.Results.First().Score;
+
+                                        cntParsed++;
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    cntErrored++;
+                                    newRow._ParsedNameValueToTheRight = " ~~~> ";
+                                    newRow._CompanyName = column.Value.ToString();
+                                    newRow._FirstName = "";
+                                    newRow._MiddleName = "";
+                                    newRow._LastName = "";
+                                    newRow._Title = "";
+                                    newRow._NickName = "";
+                                    newRow._Suffix = "";
+                                    newRow._DisplayName = column.Value.ToString();
+                                    newRow._ParseScore = -1;
+                                    errs += Environment.NewLine + "on row: " + cntRows.ToString() + ": " + ex.Message;
+                                }
+                            }
+                            outputRows.Add(newRow);
+                        }
+                    }
+                }
+
+            HaltAndCatchFire:
+
+                string outputFilename = bgParams.FilePath.Replace(".csv", "_PARSED_AT_" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".csv");
+
+                using (var writer = new StreamWriter(outputFilename))
+                {
+                    using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                    {
+                        csv.WriteRecords(outputRows);
+                    }
+                }
+
+                msg += Environment.NewLine
+                    + "FINISHED!!" + Environment.NewLine
+                    + "  File saved to: " + outputFilename + Environment.NewLine
+                    + "  Total Rows:  " + cntRows + Environment.NewLine
+                    + "  Rows Parsed: " + cntParsed + Environment.NewLine
+                    + "  Rows Errors: " + cntErrored + Environment.NewLine
+                    + "  " + (cntErrored > 0 ? " (see error details below)" + Environment.NewLine + errs + Environment.NewLine : "")
+                    + Environment.NewLine + txtStatus.Text;
+            }
+            catch (Exception ex)
+            {
+                msg = "ERROR: " + ex.Message + Environment.NewLine + ex.ToString()
+                    + Environment.NewLine
+                    + "-----------------------------------"
+                    + Environment.NewLine
+                    + txtStatus.Text;
+            }
+
+            e.Result = msg;
+        }
+
+        private void bgWorker_BinaryFog_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage <= 100)
+                progBar.Value = e.ProgressPercentage;
+        }
+
+        private void bgWorker_BinaryFog_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnParse.Enabled = true;
+            btnParse.Text = "Parse (Locally)";
+            MessageBox.Show("Done!");
+            progBar.Value = 0;
+            txtStatus.Text = e.Result.ToString();
+        }
+
+
+        #region helpers
+
+        static ExpandoObject ShallowCopy(ExpandoObject original)
+        {
+            var clone = new ExpandoObject();
+
+            var _original = (IDictionary<string, object>)original;
+            var _clone = (IDictionary<string, object>)clone;
+
+            foreach (var kvp in _original)
+                _clone.Add(kvp);
+
+            return clone;
+        }
+
+        static ExpandoObject DeepCopy(ExpandoObject original)
+        {
+            var clone = new ExpandoObject();
+
+            var _original = (IDictionary<string, object>)original;
+            var _clone = (IDictionary<string, object>)clone;
+
+            foreach (var kvp in _original)
+                _clone.Add(kvp.Key, kvp.Value is ExpandoObject ? DeepCopy((ExpandoObject)kvp.Value) : kvp.Value);
+
+            return clone;
+        }
+
+        #endregion
+    }
+}
